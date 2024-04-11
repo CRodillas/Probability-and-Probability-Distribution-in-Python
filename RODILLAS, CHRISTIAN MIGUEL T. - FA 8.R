f39eda7a7205dccca01897784b3dbb89d646128a
CRodillas <124319821+CRodillas@users.noreply.github.com>
2024-04-11 22:32:11 +0800

    Add files via upload

---
+++ RODILLAS, CHRISTIAN MIGUEL T. - FA 8.R
@@ -0,0 +1,52 @@
+mean <- 200
+variance <- 256
+
+#to find std, we can squareroot the variance
+
+std<- variance**(1/2)
+
+std
+#a
+a_over224 <- 1 -pnorm(224, mean, std)
+
+cat("a. Probability of signal over 224 µV = ", a_over224 * 100, "% \n")
+
+#b. 
+b_186to224 <-  pnorm(224, mean, std) - pnorm(186, mean, std)
+
+cat("b. Probability of signal between 186 µV to 224 µV = ", b_186to224 * 100, "% \n")
+
+#c. 
+c_below25per <- qnorm(0.25, mean, std)
+cat("c. Micro voltage below 25% = ", c_below25per, " \n")
+
+#d
+d_less240_more210 <- pnorm(240, mean, std, lower.tail = TRUE) - pnorm(210, mean, std, lower.tail = TRUE)
+cat("d. Probability of less than 240 μV, given that it is larger than 210 μV = ",d_less240_more210 *100, "% \n")
+
+#e
+interquartile_range <- qnorm(0.75, mean, std) - qnorm(0.25, mean, std)
+cat("d. Estimated interquartile range = ",interquartile_range, " \n")
+
+#f
+f_less220_more210 <- pnorm(220, mean, std, lower.tail = TRUE) - pnorm(210, mean, std, lower.tail = TRUE)
+cat("d. Probability of less than 220 μV, given that it is larger than 210 μV = ",f_less220_more210 *100, "% \n")
+
+#g
+g_more200_more220 <- (1 - pnorm(220, mean, std))/(1 - pnorm(200, mean, std))
+cat("d. Probability of more than 220 μV, given that it is larger than 200 μV = ",g_more200_more220 *100, "% \n")
+
+
+#n2
+ave_dt <- 25
+two_variance <- 144
+#to find std, we can squareroot the variance
+
+two_std <- two_variance **(1/2)
+a_bound <- qnorm(0.95, ave_dt, two_std)
+cat("a.bounds which includes 95% of the downtime = ", a_bound, "\n")
+
+b_bound <-qnorm(0.10, ave_dt, two_std, lower.tail = FALSE)
+
+cat("b.bound above which includes 10% of the downtime = ", b_bound, "\n")
+
